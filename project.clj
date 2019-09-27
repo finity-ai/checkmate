cac61ce41c8f4d50140b9c6387fbe6cd51b81847
Axel Angel <axel+git@vneko.ch>
2019-09-27 17:46:07 +0200

    Bump versions

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject cc.qbits/checkmate "0.4.0"
+(defproject cc.qbits/checkmate "0.4.1"
   :description "Retry stuff until it passes or break"
-  :url "https://github.com/mpenet/checkmate"
+  :url "https://github.com/finity-ai/checkmate"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.10.1"]
